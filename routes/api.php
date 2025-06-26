ce4851432b0a7d9ed29305ffec42c17a39d3090d
nicholasnln <nicholasnloiola@hotmail.com>
2025-06-26 13:59:00 -0300

    feat(api): add api routes for Domain

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\DomainController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+Route::get('domains', [DomainController::class, 'index']);
+Route::post('domains', [DomainController::class, 'store']);
+Route::get('domains/{id}', [DomainController::class, 'show']);
+Route::put('domains/{id}', [DomainController::class, 'update']);
