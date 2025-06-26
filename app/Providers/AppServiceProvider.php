26403d834473c03dee1a378c01151d4362238972
nicholasnln <nicholasnloiola@hotmail.com>
2025-06-26 14:00:12 -0300

    feat(app): add app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Http\Repositories\DomainRepository;
+use App\Http\Repositories\DomainRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(DomainRepositoryInterface::class, DomainRepository::class);
     }
 
     /**
